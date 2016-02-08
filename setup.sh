999c8fdd4d9bcb398771ce31a6db5603ff0b7d17
Ben Gadbois <git@bengadbois.email>
2016-02-08 19:15:53 +0100

    Adding vim plugin installations

--- setup.sh
+++ setup.sh
@@ -4,10 +4,23 @@
 
 files=(tmux.conf vimrc gitconfig)
 
-for file in ${files[@]}
+for file in "${files[@]}"
 do
   echo "setting up $file"
   ln -sf ~/dotfiles/${file} ~/.${file}
 done
+
+#vim
+
+echo "installing vim plugins"
+mkdir -p ~/.vim/autoload/ ~/.vim/bundle/
+curl -LSso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+cd ~/.vim/bundle/
+git clone https://github.com/mhinz/vim-signify
+git clone https://github.com/spolu/dwm.vim
+git clone https://github.com/ctrlpvim/ctrlp.vim
+git clone https://github.com/scrooloose/syntastic
+git clone https://github.com/tpope/vim-sleuth.git
+
 
 exit 0
