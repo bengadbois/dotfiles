5b63ddd73539d4682c5650987e99a9026e965a19
Ben Gadbois <git@bengadbois.email>
2016-10-12 11:13:17 -0700

    Adding vim-airline-themes

--- setup.sh
+++ setup.sh
@@ -32,6 +32,7 @@ git clone https://github.com/tpope/vim-sleuth.git
 git clone https://github.com/fatih/vim-go.git
 git clone https://github.com/sheerun/vim-polyglot.git
 git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
 git clone git://github.com/tpope/vim-fugitive.git
 vim -u NONE -c "helptags vim-fugitive/doc" -c q
 
