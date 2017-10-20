985643ed61037243e69792ecf27b277ff3a18083
Ben Gadbois <git@bengadbois.email>
2017-10-20 17:05:05 +0200

    Bypass brew install prompt

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ install () {
 install_mac () {
   echo "Installing packages on Mac"
   #install brew
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
 
   #fzf
   brew install fzf
