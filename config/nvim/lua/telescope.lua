b2f12d265446c6c0b0324ebb010598f0950d7797
Ben Gadbois <git@bengadbois.com>
2023-11-15 15:17:58 -0800

    vim: drop telescope zoxide example config, use vanilla

--- config/nvim/lua/telescope.lua
+++ config/nvim/lua/telescope.lua
@@ -1,5 +1,3 @@
-local z_utils = require("telescope._extensions.zoxide.utils")
-
 require('telescope').setup{
   defaults = {
   },
@@ -9,26 +7,6 @@ require('telescope').setup{
       show_untracked = true,
     }
   },
-  extensions = {
-    zoxide = {
-      -- prompt_title = "[ Walking on the shoulders of TJ ]",
-      mappings = {
-        default = {
-          after_action = function(selection)
-            print("Update to (" .. selection.z_score .. ") " .. selection.path)
-          end
-        },
-        ["<C-s>"] = {
-          before_action = function(selection) print("before C-s") end,
-          action = function(selection)
-            vim.cmd.edit(selection.path)
-          end
-        },
-        -- Opens the selected entry in a new split
-        ["<C-q>"] = { action = z_utils.create_basic_command("split") },
-      },
-    }
-  }
 }
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
