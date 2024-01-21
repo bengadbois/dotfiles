5884db71f729ef58f7ad08a4736c83994ef82643
Ben Gadbois <git@bengadbois.com>
2024-01-21 11:43:12 -0800

    fonts: switch to hack nerd font for icons

--- install.sh
+++ install.sh
@@ -69,7 +69,8 @@ install_mac () {
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   fi
 
-  brew install fzf rg tree direnv ccat jq alacritty zoxide neovim fd prettier
+  brew tap homebrew/cask-fonts
+  brew install fzf rg tree direnv ccat jq alacritty zoxide neovim fd prettier font-hack-nerd-font
   $(brew --prefix)/opt/fzf/install
 }
 
