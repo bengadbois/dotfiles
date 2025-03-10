fb793f68a93f8f373f7a950ee4893ffb8b2914f8
Ben Gadbois <git@bengadbois.com>
2025-03-10 12:16:12 -0700

    vim: setup qalc keymap

--- config/nvim/lua/plugins/calc.lua
+++ config/nvim/lua/plugins/calc.lua
@@ -3,6 +3,7 @@ return {
 		"Apeiros-46B/qalc.nvim",
 		config = function()
 			require("qalc").setup({})
+			vim.keymap.set("n", "<leader>q", "<cmd>Qalc<CR>", { desc = "Start Qalculate" })
 		end,
 	},
 }
