fedbdb71078db582caba868b42fd988bbed09635
Ben Gadbois <git@bengadbois.com>
2026-01-26 09:34:22 -0800

    vim: setup local filepath copy keymap

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -10,3 +10,9 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Cancel search" })
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+function insertFullPath()
+	local filepath = vim.fn.expand("%")
+	vim.fn.setreg("+", filepath)
+end
+vim.keymap.set("n", "<leader>fp", insertFullPath, { desc = "[F]ile [P]ath to clipboard", noremap = true })
